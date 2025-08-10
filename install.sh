68fa99fd82d57261173057d06df03a2b09696254
Sahil <smsawant17@gmail.com>
2025-08-10 07:43:11 +0530

    Remove all files

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pip install torch==2.8.0 --index-url https://download.pytorch.org/whl/cpu
-pip install -r requirements.txt --no-deps
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python server.py &
-python bridge.py
-wait
